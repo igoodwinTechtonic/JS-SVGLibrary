1607f95f588c46ba90e399fc37b6295da4789969
igoodwinTechtonic <ian.goodwin@techtonic.com>
2020-10-27 18:28:19 -0500

    Finished it

--- js/sight.js
+++ js/sight.js
@@ -6,9 +6,26 @@ class SVGElement {
         return this;
     }
     attr(attrs) {
+        // Loops through the attrs object and gets the key value pair from each
+        // Assigns each key value pair to this.node
         for (const [key, value] of Object.entries(attrs)) {
-            this.node = document.setAttributeNS(null, key, value);
+            this.node.setAttributeNS(null, key, value);
         }
         return this;
+    }
+    append(element) {
+        const parent = ( typeof(element) === 'string' ? document.querySelector(element) : element.node );
+        // Appends the node child to the parent element
+        parent.appendChild(this.node);
+        return this;
+    }
+}
+class Sight {
+    constructor(selector, width, height) {
+        // Creates new element, chains a call to attr and append to set values
+        this.svg = new SVGElement('svg').attr( {viewbox: `0 0 ${width} ${height}`} ).append(selector);
+    }
+    draw(type, attrs) {
+        return new SVGElement(type).attr(attrs).append(this.svg);
     }
 }
