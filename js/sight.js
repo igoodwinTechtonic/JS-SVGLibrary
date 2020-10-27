33206c8d7166bd7ae43898e0afe36f71b88fbfce
igoodwinTechtonic <ian.goodwin@techtonic.com>
2020-10-27 17:43:07 -0500

    First commit step 3

---
+++ js/sight.js
@@ -0,0 +1,9 @@
+class SVGElement {
+    constructor(type) {
+        this.type = type;
+        this.namespace = 'http://www.w3.org/2000/svg';
+        this.node = document.createElementNS(namespace, type);
+        return this;
+    }
+    
+}
